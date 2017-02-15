bfba2518ff6154751e2ac011f524d2dd8ce51172
carlson-matthew <pleetonboy@gmail.com>
2017-02-14 23:57:18 -0800

    remove extra folders

--- windows-enable-remote-desktop/SetupComplete2.cmd
+++
@@ -1,7 +0,0 @@
-ECHO SetupComplete2.cmd BEGIN >> %windir%\Panther\WaSetup.log
-
-ECHO Enable local admin BEGIN >> %windir%\Panther\WaSetup.log
-PowerShell.exe -ExecutionPolicy RemoteSigned "& '%~dp0EnableRemoteDesktop.ps1'"
-ECHO Enable local admin END >> %windir%\Panther\WaSetup.log
-
-ECHO SetupComplete2.cmd END >> %windir%\Panther\WaSetup.log
